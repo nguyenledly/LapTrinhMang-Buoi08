c1e1a024eb43d3cc88c032219d286acb14eb2ce6
TuanNguyen <TuanNguyen@HOANGTUAN-PC>
2018-05-08 20:48:54 +0700

    Add project files.

---
+++ Bai01/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Bai01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSender = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sender";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "To";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Subject";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Body";
+            // 
+            // txtSender
+            // 
+            this.txtSender.Location = new System.Drawing.Point(94, 25);
+            this.txtSender.Name = "txtSender";
+            this.txtSender.Size = new System.Drawing.Size(209, 20);
+            this.txtSender.TabIndex = 0;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(94, 135);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(209, 20);
+            this.txtSubject.TabIndex = 3;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(94, 95);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(209, 20);
+            this.txtTo.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(94, 60);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(209, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Location = new System.Drawing.Point(12, 224);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(73, 35);
+            this.btnAttach.TabIndex = 5;
+            this.btnAttach.Text = "Attachment";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(230, 321);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(73, 35);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(94, 171);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(209, 115);
+            this.txtBody.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 390);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnAttach);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtSender);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Send Mail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSender;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtBody;
+    }
+}
+
---
+++ Bai01/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Bai01
+{
+    public partial class Form1 : Form
+    {
+        SmtpClient mailclient = new SmtpClient("smtp.gmail.com", 587);
+        MailMessage message = null;
+        string filePath = "";
+        public Form1()
+        {
+            mailclient.EnableSsl = true;
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((txtSender.Text == string.Empty) || (txtPass.Text == string.Empty) || (txtTo.Text == string.Empty) || (txtBody.Text == string.Empty))
+                    MessageBox.Show("Information is not empty!");
+                else
+                {
+                    mailclient.Credentials = new NetworkCredential(txtSender.Text, txtPass.Text);
+                    message = new MailMessage(txtSender.Text, txtTo.Text);
+                    message.Subject = txtSubject.Text;
+                    message.Body = txtBody.Text;
+                    if (filePath == "")
+                        return;
+                    else
+                    {
+                        Attachment attach = new Attachment(filePath);
+                        message.Attachments.Add(attach);
+                    }
+                    mailclient.Send(message);
+                    MessageBox.Show("Mail was sent successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = @"C:\";
+            open.Title = "Browse a file";
+            open.Filter = "All files(*.*)|*.*";
+            if (open.ShowDialog() == DialogResult.OK)
+                filePath = open.FileName;
+            txtBody.Text += "Attachment: " + filePath;
+
+        }
+    }
+}
